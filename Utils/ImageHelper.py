ab71c195e5461d6f28db1056cc5220c2dbaa9b78
Imesha Sudasingha <imesha.sudasinghe@gmail.com>
2016-05-25 23:30:58 +0530

    Rotate image feature added

--- Utils/ImageHelper.py
+++ Utils/ImageHelper.py
@@ -1,21 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageTk
 import numpy as np
 
 
 class ImageHelper:
-    def __init__(self, image_name):
-        self.image = Image.open(image_name)
-        self.width, self.height = self.image.size
-        self.pixels = False
-
-    # Load the image and store all the pixels.
-    def load_image(self):
-        self.pixels = np.zeros((self.width, self.height, 3))
-        pixels = self.image.load()
-        for i in range(self.width):
-            for j in range(self.height):
-                r, g, b = pixels[i, j]
-                self.pixels[i, j, :] = r, g, b
+    # pixels array is in the form [height,width,(r g b)]
+    def __init__(self, image_name=""):
+        if len(image_name) != 0:
+            self.image = Image.open(image_name)
+            self.pixels = np.array(self.image)
+            self.width, self.height = self.image.size
 
     def print_image(self):
         print(self.pixels)
+
+    def get_photo_image(self):
+        return ImageTk.PhotoImage(self.image);
+
+    # height->width, width->height. shape[0]=>height, shape[1]=>width
+    def rotate_left(self):
+        new_image = np.zeros((self.pixels.shape[1], self.pixels.shape[0], 3))
+        for i in range(self.pixels.shape[0]):
+            for j in range(self.pixels.shape[1]):
+                new_image[j, i, :] = self.pixels[i, j, :]
+        self.pixels = new_image
+        self.image = Image.fromarray(np.uint8(new_image))
--- window.py
+++ window.py
@@ -1,16 +1,54 @@
-from tkinter import *
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
 from Utils.ImageHelper import ImageHelper
 
-window = Tk()
-window.geometry("500x500")
+# Image Helper instance to be used through out the application
+imageHelper = False
 
 
+# Load the image from a file picker when the button is clicked
 def load_image():
-    image = ImageHelper("/home/imesha/Downloads/Technology-001.jpg")
-    image.load_image()
-    image.print_image()
+    global imageHelper
+    image_name = askopenfilename(title="Select an Image",
+                                 filetypes=[('all files', '.*'), ('image files', '.jpg')])
+    if len(image_name) > 0:
+        imageHelper = ImageHelper(image_name)
+        image = imageHelper.get_photo_image()
+        set_image(image)
 
 
-button = Button(window, text="Load Image", command=load_image)
-button.place(x=10, y=10)
+# Rotate the image left
+def rotate_left():
+    if imageHelper:
+        imageHelper.rotate_left()
+        image = imageHelper.get_photo_image()
+        set_image(image)
+
+
+def set_image(image):
+    imageLabel.configure(image=image)
+    imageLabel.image = image
+
+
+window = tk.Tk()
+# Screen size
+screen_size = window.winfo_screenwidth(), window.winfo_screenheight()
+# Set the geometry of the screen. 100 pixels less from actual size of the screen
+window.geometry(str(screen_size[0] - 200) + "x" + str(screen_size[0] - 200))
+# Frame on the top. This will be used to shw buttons.
+topFrame = tk.Frame(window)
+topFrame.pack()
+# Frame on bottom. This will be used to show the loaded image.
+bottomFrame = tk.Frame(window)
+bottomFrame.pack()
+# add load button
+loadButton = tk.Button(topFrame, text="Load Image", command=load_image)
+loadButton.pack(side=tk.LEFT)
+# add rotate button
+rotateButton = tk.Button(topFrame, text="Rotate Left", command=rotate_left)
+rotateButton.pack(side=tk.LEFT)
+# Add label to show image
+imageLabel = tk.Label(bottomFrame)
+imageLabel.pack(side=tk.BOTTOM)
+
 window.mainloop()
